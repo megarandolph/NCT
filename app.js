b24b83db5cfc9403b2d7dcad317a44b2a30d8433
Randolph Marte <randolphmarte@hotmail.com>
2022-06-30 01:35:26 -0400

    First structure of project, and get of transactions was implement.

---
+++ app.js
@@ -0,0 +1,102 @@
+const transactions = []
+  
+const updateFormButton = document.querySelector("button#updateitem")
+const createButton = document.querySelector("button#createitem")
+const mainDiv = document.querySelector("main") 
+  
+  ///////////////////////
+  // Functions
+  ///////////////////////
+  const GetTransactions = () =>{
+
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "fa0d372d-a00f-47c5-b945-22b22005a669");
+
+    var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+    };
+
+    fetch("http://63.135.170.173:5000/transactions", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+        if(result.success){
+            var data = result.data;
+            for(var i=0; i < data.length; i++){
+
+                const transaction = {data:data[i]}
+                transactions.push(transaction)
+                
+            }
+            console.log(data)
+            renderData()
+        }else{
+            console.log(result.errorMessage)
+        }
+    }
+    )
+    .catch(error => console.log('error', error));
+  }
+  
+  const renderData = () => {
+    
+    mainDiv.innerHTML = ""
+  
+
+    transactions.forEach((transaction, index) => {
+      const transactionDiv = document.createElement("div")// Creates new h1 element
+        transactionDiv.className = "card text-white bg-dark mb-3 col-md-4"
+        transactionDiv.setAttribute("style","max-width: 18rem;")
+
+      transactionDiv.innerHTML = 
+      `<div class="card-header">${transaction.data.concept}</div>
+      <div class="card-body">
+        <h5 class="card-title">${transaction.data.ammount}</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>` 
+
+      const ButtonContainer = document.createElement("div")
+      ButtonContainer.className = "row justify-content-center"
+
+      const deleteButton = document.createElement(`button`)
+      deleteButton.id = index
+      deleteButton.className = "btn btn-danger col-md-4"
+      deleteButton.innerText = "Delete" 
+      deleteButton.addEventListener("click", event => {
+        transactions.splice(index, 1) 
+        renderData() 
+      })
+      ButtonContainer.appendChild(deleteButton) 
+  
+
+      const updateButton = document.createElement(`button`) 
+      updateButton.id = index
+      updateButton.className = "btn btn-primary col-md-4"
+      updateButton.innerText = "Update" 
+      updateButton.addEventListener("click", event => {
+        
+      })
+      ButtonContainer.appendChild(updateButton) 
+
+      transactionDiv.appendChild(ButtonContainer)
+
+      mainDiv.appendChild(transactionDiv) 
+    })
+  }
+  
+  const createData = () => {
+    
+    renderData() 
+  }
+  
+  const updateData = event => {
+    
+    renderData() 
+  }
+  
+  ////////////////////
+  // Main App Logic
+  ////////////////////
+
+  GetTransactions()
