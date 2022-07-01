4929b2724ba4652f8cd2e2667c8a653a2a829ce5
Randolph Marte <randolphmarte@hotmail.com>
2022-07-01 01:12:43 -0400

    Dropdown list of Account apis was added. Post and Delete transaction were added. Form modal was completed. Post form validation was added. Error message when try to delete some transaction and you wasn't created.

--- app.js
+++ app.js
@@ -33,8 +33,7 @@ const SelectAccount = document.querySelector("Select#AccountId")
             for(var i=0; i < data.length; i++){
                 transactions.push(data[i])
                 
-            }
-            console.log(transactions)
+            }       
             renderData()           
         }else{
             console.log(result.errorMessage)
@@ -48,6 +47,78 @@ const SelectAccount = document.querySelector("Select#AccountId")
    var transactionDetail = transactions.find(x => x.id === id);
     const myTransactionString = JSON.stringify(transactionDetail);
     localStorage.setItem('transactionDetail', myTransactionString);
+  }
+
+  const GetAccounts = () =>{
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "fa0d372d-a00f-47c5-b945-22b22005a669");
+
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    };
+
+    fetch("http://63.135.170.173:5000/accounts", requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        if(result.success){
+          var data = result.data;
+          for(var i=0; i < data.length; i++){
+            const option = document.createElement('option');
+            option.text = data[i].type+' '+data[i].currency;
+            option.value = data[i].id;
+            SelectAccount.add(option);
+              
+          }      
+          }else{
+          console.log(result.errorMessage)
+          }
+        })
+      .catch(error => console.log('error', error));
+  }
+  
+  const PostTransaction = (formdata) =>{
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "fa0d372d-a00f-47c5-b945-22b22005a669");
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: formdata,
+      redirect: 'follow'
+    };
+
+    fetch("http://63.135.170.173:5000/transactions", requestOptions)
+      .then(response => response.JSON())
+      .then(result =>{
+        if(result.success){
+          swal("Successfully saved!", "click here to ok!", "success");
+        }
+      })
+      .catch(error => console.log('error', error));
+  }
+
+  const DeleteTransaction = (id)=>{
+    var myHeaders = new Headers();
+        myHeaders.append("Authorization", "fa0d372d-a00f-47c5-b945-22b22005a669");
+
+        var requestOptions = {
+          method: 'DELETE',
+          headers: myHeaders,
+          redirect: 'follow'
+        };
+
+        fetch("http://63.135.170.173:5000/transactions/:"+id, requestOptions)
+          .then(response => response.text())
+          .then(result => {
+            if(result.success){
+              GetTransactions();
+            }else{
+              swal("You don't", "you not have permission to delete this element", "error");
+            }
+          })
+          .catch(error => console.log('error', error));
   }
 
   const renderData = () => {
@@ -68,13 +139,11 @@ const SelectAccount = document.querySelector("Select#AccountId")
       ButtonContainer.className = "row justify-content-center"
 
       const deleteButton = document.createElement(`button`)
-      deleteButton.id = index
+      deleteButton.id = transaction.id
       deleteButton.className = "btn btn-danger col-md-4 m-2"
       deleteButton.innerText = "Delete" 
-      deleteButton.addEventListener("click", event => {
-        transactions.splice(index, 1) 
-        renderData() 
-        
+      deleteButton.addEventListener("click", event => {    
+        DeleteTransaction(deleteButton.id);
       })
       ButtonContainer.appendChild(deleteButton) 
   
@@ -126,11 +195,47 @@ const SelectAccount = document.querySelector("Select#AccountId")
   ////////////////////
   // Main App Logic
   ////////////////////
-  
-
   if(window.location.pathname.split("/").pop() == "Home.html"){
     GetTransactions()
+    GetAccounts()
+
+    document.querySelector("button#myInput").addEventListener("click", event => {
+      $("#MyModal").modal('show');
+    })
+    document.querySelector("button#ModalClose").addEventListener("click", event => {
+      $("#MyModal").modal('hide');
+    })    
+
+    const forms = document.querySelectorAll('.needs-validation')
+    Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {       
+        event.preventDefault()
+        event.stopPropagation()
+
+      }else{
+        event.preventDefault()
+        var formdata = new FormData();
+        formdata.append("concept", document.querySelector('input#Concept').value);
+        formdata.append("description", document.querySelector('input#Description').value);
+        formdata.append("ammount", document.querySelector('input#Ammount').value);
+        formdata.append("date", document.querySelector('input#Date').value);
+        formdata.append("accountId", document.querySelector('select#AccountId').value);
+        formdata.append("candidateId", "fa0d372d-a00f-47c5-b945-22b22005a669");
+        PostTransaction(formdata);
+        
+      }
+      form.classList.add('was-validated')
+
+    }, false)});
   }
   else{
     renderDataDetail()
   }
+
+ 
+
+  
+
+
+  
